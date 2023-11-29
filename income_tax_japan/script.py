af06d2ffbefca9f8e4e7d4fb448fe6da699314cc
Krishna Soni <k-soni@ispace-inc.com>
2023-11-29 11:59:45 +0900

    fix(script): changed the name to x to remain anonymous

--- income_tax_japan/script.py
+++ income_tax_japan/script.py
@@ -1,9 +1,9 @@
 from income_tax_japan.classes import Employee
 
 if __name__ == "__main__":
-    krishna = Employee(5360810, 0)
-    krishna_old = Employee(5000000, 0)
-    krishna_dependant = Employee(5360810, 1)
-    print(krishna.income_tax / 12)
-    print(krishna_old.income_tax / 12)
-    print(krishna.income_tax - krishna_dependant.income_tax)
+    employee_x = Employee(5360810, 0)
+    employee_x_old = Employee(5000000, 0)
+    employee_x_dependant = Employee(5360810, 1)
+    print(employee_x.income_tax / 12)
+    print(employee_x_old.income_tax / 12)
+    print(employee_x.income_tax - employee_x_dependant.income_tax)
